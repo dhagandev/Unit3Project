83d95f43426f36f21e7b0e25954afc19cd863ea6
dhagandev <dhagan@utexas.edu>
2019-06-14 14:44:10 -0500

    Set up env related variables and secrets

--- ConwayProject/ConwayProject/settings.py
+++ ConwayProject/ConwayProject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+
+environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,8 +80,9 @@ WSGI_APPLICATION = 'ConwayProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ConwayProject',
+        'PORT': os.environ['PSQL_PORT']
     }
 }
 
