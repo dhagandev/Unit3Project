4f4cc0c7365c694451f65cc7ad410eb894c38797
dhagandev <dhagan@utexas.edu>
2019-06-14 16:18:21 -0500

    Routing to basic pages

---
+++ ConwayProject/conway_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('rules/', views.rules, name='rules'),
+    path('sim/create/', views.consim_create, name='create'),
+    path('sim/<int:consim_id>/', views.consim_detail, name='detail'),
+    path('users/<int:user_id>/', views.user_profile, name='profile'),
+]
--- ConwayProject/conway_app/views.py
+++ ConwayProject/conway_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+	return render(request, 'home.html')
+
+def rules(request):
+	return render(request, 'rules.html')
+
+def consim_create(request):
+	return render(request, 'consim/createconsim.html')
+
+def consim_detail(request):
+	return render(request, 'consim/viewconsim.html')
+
+def user_profile(request):
+	user = User.objects.get(id=user_id)
+	return render(request, 'users/profile.html', {'user': user})
