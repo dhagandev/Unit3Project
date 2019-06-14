ee85fc952a9b8afe72b6cf65bb2a27ef25614627
Jenny Martin <jennymartin@Jennys-MacBook-Pro.local>
2019-06-14 16:32:01 -0500

    auth updates

--- ConwayProject/conway_app/models.py
+++ ConwayProject/conway_app/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-
 from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
 
-#update model to include FK and on_delete=models.CASCADE?
+    def __str__(self):
+        return self.name
